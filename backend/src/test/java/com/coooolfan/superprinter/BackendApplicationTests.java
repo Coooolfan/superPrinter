0dfb51d0725163d9e829291e794d15fe73f76ee6
YangYiFan <Coolfan1024@gmail.com>
2025-04-23 00:02:02 +0800

    feat: 测试lua脚本交互

--- backend/src/test/java/com/coooolfan/superprinter/BackendApplicationTests.java
+++ backend/src/test/java/com/coooolfan/superprinter/BackendApplicationTests.java
@@ -4,18 +4,24 @@ import com.coooolfan.superprinter.util.Minio.MinioUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+
+import java.util.List;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Autowired
-    private MinioUtil minioUtil;
+    private RedisTemplate redisTemplate;
 
     @Test
     void contextLoads() throws Exception {
-//        System.out.println(minioUtil.getPresignedUploadUrl("test.txt", 6000));
-//        System.out.println(minioUtil.listAllFile());
-        System.out.println(minioUtil.getPresignedDownloadUrl("test.txt", 6000));
+        DefaultRedisScript<String> CREATE_ORDER_LOCK_SCRIPT = new DefaultRedisScript<>();
+        CREATE_ORDER_LOCK_SCRIPT.setScriptText("return redis.call('HGET', KEYS[1], 'pageCount')");
+        CREATE_ORDER_LOCK_SCRIPT.setResultType(String.class);
+        String hashKey = "idempotent:330f6ddc-8f77-4bd7-8ede-b46b1057c310";
+        System.out.println(redisTemplate.execute(CREATE_ORDER_LOCK_SCRIPT, List.of(hashKey)));
     }
 
 }
